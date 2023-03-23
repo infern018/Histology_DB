7c74b619f9c0a637513aaf2917db4daa74ad6216
infern018 <singh.vishu234@gmail.com>
2023-03-23 14:03:33 +0530

    [Fix]:Shared collections crumbling request bug

---
+++ client/src/pages/entries/EntryCreate.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import EntryForm from '../../components/Form/EntryForm';
+import Navbar from '../../components/Navbar/Navbar';
+import axios from 'axios'
+import { useSelector } from 'react-redux';
+
+const EntryCreate = () => {
+    const location = useLocation();
+    const collectionID = location.pathname.split("/")[2];
+
+    const user = useSelector(state=>state.user.currentUser)
+
+    const handleEntryCreate = async (entry) => {
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/entries/${collectionID}/${user._id}`,entry,
+            {
+                headers:{
+                    'token':`Bearer ${user.accessToken}`
+                }
+            })
+            console.log("ADDED SUCCESSFULLY",res.data)
+        } catch (error) {
+            console.log("ERROR",error)
+        }
+    }
+
+  return (
+    <div>
+        <Navbar/>
+        <EntryForm collectionID={collectionID} onFormSubmit={(entry)=>handleEntryCreate(entry)}/>
+    </div>
+  )
+}
+
+export default EntryCreate
---
+++ server/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const dotenv  = require('dotenv')
+const mongoose = require('mongoose')
+
+//ROUTER
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');    
+const entryRoute = require('./routes/entries');
+const collectionRoute = require('./routes/collections');
+const roleRoute = require('./routes/roles');
+
+const cors = require('cors')
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/views'));
+app.use(cors())
+
+
+// parse application/json
+app.use(bodyParser.json())
+
+dotenv.config(
+    {
+        path: "PROD" === process.env.NODE_ENV?.toUpperCase() ? '../.env.prod' : '../.env.dev'
+    }
+)
+
+mongoose
+    .connect(process.env.MONGO_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+    .then(console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err))
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/entries",entryRoute)
+app.use("/api/collections",collectionRoute)
+app.use("/api/roles",roleRoute)
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on PORT ", PORT);
+})
