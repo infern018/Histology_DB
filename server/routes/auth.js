457c359f2b296fc1567c12d10e1c2abd3e350534
Vishwas <singh.vishu234@gmail.com>
2024-08-28 00:49:16 +0530

    Feat : Delete duplicate client code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -14,8 +14,14 @@ router.get("/github", passport.authenticate("github"));
 router.get("/github/callback",
     passport.authenticate("github", { failureRedirect: "/" }),
     (req, res) => {
+        // only extract username, githubId
+        userData = {
+            username: req.user.username,
+            githubId: req.user.githubId
+        }
+
         // Redirect to your frontend application with user data in session or through a URL parameter
-        res.redirect(`${config.react_url}/login/github?user=${encodeURIComponent(JSON.stringify(req.user))}`); // Use encodeURIComponent to safely encode the user data
+        res.redirect(`${config.react_url}/login/github?user=${encodeURIComponent(JSON.stringify(userData))}`); // Use encodeURIComponent to safely encode the user data
     }
 );
 
